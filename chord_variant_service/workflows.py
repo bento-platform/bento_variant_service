c769260d6b1f51171cfe063cf541c6bdfb6b94d8
David Lougheed <david.lougheed@gmail.com>
2019-10-04 16:11:14 -0400

    Switch to chord_lib-defined workflow utils

--- chord_variant_service/workflows.py
+++ chord_variant_service/workflows.py
@@ -1,5 +1,5 @@
+from chord_lib.workflows import get_workflow, get_workflow_resource, workflow_exists
 from flask import Blueprint, current_app, json, jsonify
-from werkzeug.utils import secure_filename
 
 bp_workflows = Blueprint("workflows", __name__)
 
@@ -14,33 +14,20 @@ def workflow_list():
     return jsonify(WORKFLOWS)
 
 
-def workflow_exists(workflow_name):
-    return workflow_name in WORKFLOWS["ingestion"] or workflow_name in WORKFLOWS["analysis"]
-
-
-def get_workflow(workflow_name):
-    return (WORKFLOWS["ingestion"][workflow_name] if workflow_name in WORKFLOWS["ingestion"]
-            else WORKFLOWS["analysis"][workflow_name])
-
-
-def get_workflow_resource(workflow_name):
-    return "workflows/{}".format(secure_filename(get_workflow(workflow_name)["file"]))
-
-
 @bp_workflows.route("/workflows/<string:workflow_name>", methods=["GET"])
 def workflow_detail(workflow_name):
     # TODO: Better errors
-    if not workflow_exists(workflow_name):
+    if not workflow_exists(workflow_name, WORKFLOWS):
         return current_app.response_class(status=404)
 
-    return jsonify(get_workflow(workflow_name))
+    return jsonify(get_workflow(workflow_name, WORKFLOWS))
 
 
 @bp_workflows.route("/workflows/<string:workflow_name>.wdl", methods=["GET"])
 def workflow_wdl(workflow_name):
     # TODO: Better errors
-    if not workflow_exists(workflow_name):
+    if not workflow_exists(workflow_name, WORKFLOWS):
         return current_app.response_class(status=404)
 
-    with current_app.open_resource(get_workflow_resource(workflow_name)) as wfh:
+    with current_app.open_resource("workflows/{}".format(get_workflow_resource(workflow_name, WORKFLOWS))) as wfh:
         return current_app.response_class(response=wfh.read(), mimetype="text/plain", status=200)
