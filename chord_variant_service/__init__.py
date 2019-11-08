786570a26d401df4cf428a9d7e6b03cb2b18605c
David Lougheed <david.lougheed@gmail.com>
2019-11-08 16:53:31 -0500

    Get version using chord_lib util

--- chord_variant_service/__init__.py
+++ chord_variant_service/__init__.py
@@ -1,16 +1,7 @@
-#!/usr/bin/env python3
-
 import os
-import re
 
+from chord_lib.utils import get_own_version
 from pathlib import Path
-from pkg_resources import get_distribution, DistributionNotFound
 
 name = "chord_variant_service"
-
-try:
-    __version__ = get_distribution(name).version
-except DistributionNotFound:
-    setup_path = os.path.join(Path(os.path.dirname(os.path.realpath(__file__))).parent, "setup.py")
-    __version__ = [re.search(r"(\d+\.\d+\.\d+)", l).group(1) for l in open(setup_path, "r").readlines()
-                   if "    version" in l][0]
+__version__ = get_own_version(os.path.join(Path(os.path.dirname(os.path.realpath(__file__))).parent, "setup.py"), name)
