f2cd1b7743c0a84503cee454f6d27358e4ceaf7d
David Lougheed <david.lougheed@gmail.com>
2020-01-08 16:33:28 -0500

    Add auth headers to ingest tests

--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -15,11 +15,14 @@ def make_ingest(dataset_id):
     }
 
 
+TEST_HEADERS = {"X-User": "test", "X-User-Role": "owner"}
+
+
 def test_ingest(client):
-    rv = client.post("/private/ingest", json={})
+    rv = client.post("/private/ingest", json={}, headers=TEST_HEADERS)
     assert rv.status_code == 400
 
-    rv = client.post("/private/ingest", json=make_ingest("invalid_dataset_id"))
+    rv = client.post("/private/ingest", json=make_ingest("invalid_dataset_id"), headers=TEST_HEADERS)
     assert rv.status_code == 400
 
     # TODO: Test workflow ID validation
