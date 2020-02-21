56c8db35660d65db6affa4d643499b35bcefbe5d
David Lougheed <david.lougheed@gmail.com>
2020-02-20 19:18:11 -0500

    Update chord_lib to 0.4.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
 
     python_requires=">=3.6",
     install_requires=[
-        "chord_lib[flask]==0.3.0",
+        "chord_lib[flask]==0.4.0",
         "Flask>=1.1,<2.0",
         "jsonschema>=3.2,<4.0",
         "pysam>=0.15,<0.16",
