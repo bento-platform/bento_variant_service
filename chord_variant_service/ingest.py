3a124984f9c2548a86a84308c0d5003087544253
David Lougheed <david.lougheed@gmail.com>
2020-03-18 12:04:52 -0400

    Support different table managers for ingest endpoint

--- chord_variant_service/ingest.py
+++ chord_variant_service/ingest.py
@@ -1,6 +1,9 @@
+import json
 import os
 import shutil
+import sys
 
+from base64 import urlsafe_b64encode
 from chord_lib.ingestion import (
     WORKFLOW_TYPE_FILE,
     WORKFLOW_TYPE_FILE_ARRAY,
@@ -15,34 +18,80 @@ from chord_lib.schemas.chord import CHORD_INGEST_SCHEMA
 from chord_lib.workflows import workflow_exists
 from flask import Blueprint, current_app, request
 from jsonschema import validate, ValidationError
+from typing import List, Tuple
+from urllib.parse import urlparse
 
-from chord_variant_service.constants import DATA_PATH
-from chord_variant_service.table_manager import MANAGER_TYPE_VCF, get_table_manager
+from chord_variant_service.constants import DATA_PATH, SERVICE_NAME
+from chord_variant_service.table_manager import (
+    MANAGER_TYPE_DRS,
+    MANAGER_TYPE_MEMORY,
+    MANAGER_TYPE_VCF,
+    get_table_manager,
+)
 from chord_variant_service.workflows import WORKFLOWS
 
 
 bp_ingest = Blueprint("ingest", __name__)
 
 
-def get_ingest_metadata_from_request():
-    workflow_id = request.json["workflow_id"].strip()
-    workflow_metadata = request.json["workflow_metadata"]
-    workflow_outputs = request.json["workflow_outputs"]
-    workflow_params = request.json["workflow_params"]
+WORKFLOW_OUTPUT_VCF_GZ_FILES = "vcf_gz_files"
+WORKFLOW_OUTPUT_TBI_FILES = "tbi_files"
+
+
+MEMORY_CANNOT_INGEST_ERROR = "Cannot ingest into a memory-based table manager"
+
+
+def get_ingest_metadata_from_request(request_data):
+    workflow_id = request_data["workflow_id"].strip()
+    workflow_metadata = request_data["workflow_metadata"]
+    workflow_outputs = request_data["workflow_outputs"]
+    workflow_params = request_data["workflow_params"]
     return workflow_id, workflow_metadata, workflow_outputs, workflow_params
 
 
-def move_ingest_files(table_id: str):
-    workflow_id, workflow_metadata, workflow_outputs, workflow_params = get_ingest_metadata_from_request()
+def write_drs_object_files(table_id: str, request_data: dict):
+    workflow_outputs = get_ingest_metadata_from_request(request_data)[3]
+    table_path = os.path.join(DATA_PATH, table_id)
+
+    # Fetch the relevant files from the workflows (these should be DRS URLs...)
+    vcfs: List[str] = workflow_outputs.get(WORKFLOW_OUTPUT_VCF_GZ_FILES, [])
+    tbis: List[str] = workflow_outputs.get(WORKFLOW_OUTPUT_TBI_FILES, [])
+
+    if len(vcfs) != len(tbis):
+        print(f"[{SERVICE_NAME}] Mismatched VCF GZ and TBI array lengths: {len(vcfs)}, {len(tbis)}")
+        return
+
+    pairs: Tuple[Tuple[str, str], ...] = tuple(zip(vcfs, tbis))
+
+    for vcf_url, idx_url in pairs:
+        v = urlparse(vcf_url)
+        i = urlparse(idx_url)
+        if v.scheme != "drs" or i.scheme != "drs":
+            print(f"[{SERVICE_NAME}] Invalid DRS URL: {vcf_url} or {idx_url}")
+            continue
+
+        # TODO: Check hashes for duplicate files
+
+        drs_object_file_path = os.path.join(table_path, f"{urlsafe_b64encode(vcf_url)}.drs.json")
+        if os.path.exists(drs_object_file_path):
+            continue
+
+        with open(drs_object_file_path, "w") as df:
+            json.dump({"data": vcf_url, "index": idx_url}, df)
+
+
+def move_ingest_files(table_id: str, request_data: dict):
+    workflow_id, workflow_metadata, workflow_outputs, workflow_params = get_ingest_metadata_from_request(request_data)
+    table_path = os.path.join(DATA_PATH, table_id)
 
     output_params = make_output_params(workflow_id, workflow_params, workflow_metadata["inputs"])
-    prefix = find_common_prefix(os.path.join(DATA_PATH, table_id), workflow_metadata, output_params)
+    prefix = find_common_prefix(table_path, workflow_metadata, output_params)
 
     def ingest_file_path(f):
         # Full path to to-be-newly-ingested file
         #  - Rename file if a duplicate name exists (ex. dup.vcf.gz becomes 1_dup.vcf.gz)
         #  - If prefix is None, it will not be added
-        return os.path.join(DATA_PATH, table_id, file_with_prefix(f, prefix))
+        return os.path.join(table_path, file_with_prefix(f, prefix))
 
     files_to_move = []
 
@@ -74,7 +123,7 @@ def ingest():
         if table_id not in get_table_manager().get_tables():
             return flask_errors.flask_bad_request_error(f"No table with ID: {table_id}")
 
-        workflow_id, workflow_metadata, workflow_outputs, _ = get_ingest_metadata_from_request()
+        workflow_id, workflow_metadata, workflow_outputs, _ = get_ingest_metadata_from_request(request.json)
 
         if not workflow_exists(workflow_id, WORKFLOWS):
             # Check that the workflow exists here...
@@ -84,15 +133,25 @@ def ingest():
         # TODO: Make sure DRS support works
         # TODO: Support memory variant loading (from VCFs?)
 
+        # TODO: More extensive, standardized, chord_lib-based validation of workflow ingestion data
         for output in workflow_metadata["outputs"]:
             if output["id"] not in workflow_outputs:
                 # Missing output
-                print("Missing {} in {}".format(output["id"], workflow_outputs))
-                return current_app.response_class(status=400)
+                err = f"Missing output {output['id']} in {workflow_id} (Outputs: {workflow_outputs})"
+                print(f"[{SERVICE_NAME}] {err}", file=sys.stderr, flush=True)
+                return flask_errors.flask_bad_request_error(err)
 
-        if current_app.config["TABLE_MANAGER"] == MANAGER_TYPE_VCF:
-            move_ingest_files(table_id)
+        # Check manager type to determine how the ingestion will be handled
+        manager_type = current_app.config["TABLE_MANAGER"]
+        if manager_type == MANAGER_TYPE_DRS:
+            write_drs_object_files(table_id, request.json)
+        elif manager_type == MANAGER_TYPE_MEMORY:
+            print(f"[{SERVICE_NAME}] Unsupported: {MEMORY_CANNOT_INGEST_ERROR}", file=sys.stderr, flush=True)
+            return flask_errors.flask_bad_request_error(MEMORY_CANNOT_INGEST_ERROR)
+        elif manager_type == MANAGER_TYPE_VCF:
+            move_ingest_files(table_id, request.json)
 
+        # After files have been handled, refresh the tables in the manager
         get_table_manager().update_tables()
 
         return current_app.response_class(status=204)
