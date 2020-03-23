183479458043b6ac28cde1f01c03919b8d6b62a4
David Lougheed <david.lougheed@gmail.com>
2020-03-23 16:29:51 -0400

    Add some testing for VCF variant tables

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,9 +10,26 @@ from chord_variant_service.app import application
 def app():
     application.config["TESTING"] = True
     application.config["TABLE_MANAGER"] = "memory"
+
     with application.app_context():
         tm._table_manager = None
         tm.get_table_manager()
+    yield application
+
+
+@pytest.fixture
+def vcf_app(tmpdir):
+    data_path = tmpdir / "vcf_data"
+    data_path.mkdir()
+
+    application.config["TESTING"] = True
+    application.config["DATA_PATH"] = str(data_path)
+    application.config["TABLE_MANAGER"] = "vcf"
+
+    with application.app_context():
+        tm._table_manager = None
+        tm.get_table_manager()
+
     yield application
 
 
@@ -22,9 +39,20 @@ def table_manager(app):
         yield tm.get_table_manager()
 
 
+@pytest.fixture()
+def vcf_table_manager(vcf_app):
+    with vcf_app.app_context():
+        yield tm.get_table_manager()
+
+
 @pytest.fixture
 def client(app):
     yield app.test_client()
+
+
+@pytest.fixture
+def vcf_client(vcf_app):
+    yield vcf_app.test_client()
 
 
 @pytest.fixture(scope="module")
--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -9,18 +9,22 @@ def make_ingest(dataset_id, workflow_id="", workflow_outputs=None, workflow_para
 
 TEST_HEADERS = {"X-User": "test", "X-User-Role": "owner"}
 
+# TODO: Test errors better
 
-def test_ingest(client):
-    # TODO: Test errors better
 
+def test_ingest_no_body(client):
     # No ingest body
     rv = client.post("/private/ingest", json={}, headers=TEST_HEADERS)
     assert rv.status_code == 400
 
+
+def test_ingest_bad_table(client):
     # Non-existant table
     rv = client.post("/private/ingest", json=make_ingest("invalid_table_id"), headers=TEST_HEADERS)
     assert rv.status_code == 400
 
+
+def test_ingest_bad_requests(client):
     # Create a dummy table
     client.post("/tables?data-type=variant", json={
         "name": "test table",
@@ -42,6 +46,8 @@ def test_ingest(client):
     }), headers=TEST_HEADERS)
     assert rv.status_code == 400
 
+
+def test_ingest_memory(client):
     # Valid workflow ID, unsupported table manager type for ingestion
     rv = client.post("/private/ingest", json=make_ingest("fixed_id", "vcf_gz", {
         "vcf_gz_files": [],
@@ -52,3 +58,44 @@ def test_ingest(client):
     # TODO: Test workflow ID validation - analysis vs ingestion
     # TODO: Test workflow parameters / outputs
     # TODO: Test functional ingestion
+
+
+EMPTY_WORKFLOW_OUTPUTS = {
+    "vcf_gz_files": [],
+    "tbi_files": []
+}
+
+
+def test_ingest_vcf(vcf_client):
+    # Create a dummy table
+    tr = vcf_client.post("/tables?data-type=variant", json={
+        "name": "test table",
+        "metadata": {}
+    })
+    t = tr.get_json()
+
+    # TODO: Test bad params - this currently yields a 500; it should be a 400
+
+    # Valid workflow ID, invalid params (missing prefix)
+    rv = vcf_client.post("/private/ingest", json=make_ingest(
+        t["id"],
+        "vcf_gz",
+        workflow_outputs=EMPTY_WORKFLOW_OUTPUTS,
+        workflow_params={
+            "vcf_gz_files": [],
+            "assembly_id": "GRCh38"
+        }
+    ), headers=TEST_HEADERS)
+    assert rv.status_code == 400
+
+    # Valid workflow ID, empty file arrays
+    rv = vcf_client.post("/private/ingest", json=make_ingest(
+        t["id"],
+        "vcf_gz",
+        workflow_outputs=EMPTY_WORKFLOW_OUTPUTS,
+        workflow_params={
+            "vcf_gz.vcf_gz_files": [],
+            "vcf_gz.assembly_id": "GRCh38"
+        }
+    ), headers=TEST_HEADERS)
+    assert rv.status_code == 204
