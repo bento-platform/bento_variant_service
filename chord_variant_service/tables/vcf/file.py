851d602ccc809c6043c9494d644b2a615809f162
David Lougheed <david.lougheed@gmail.com>
2020-05-01 13:35:59 -0400

    Revert

--- chord_variant_service/tables/vcf/file.py
+++ chord_variant_service/tables/vcf/file.py
@@ -37,11 +37,11 @@ class VCFFile:
 
         # Find row count
         try:
-            # p = subprocess.Popen(("bcftools", "index", "--nrecords", vcf_path), stdout=subprocess.PIPE)
-            f = pysam.TabixFile(self.path, index=self.index_path, parser=pysam.asTuple(), threads=WORKERS)
-            self._n_of_variants: int = sum(1 for _ in f.fetch())  # TODO: Make this more efficient
-            f.close()
-            # self._n_of_variants: int = int(p.stdout.read().strip())  # TODO: Handle error
+            p = subprocess.Popen(("bcftools", "index", "--nrecords", vcf_path), stdout=subprocess.PIPE)
+            # f = pysam.TabixFile(self.path, index=self.index_path, parser=pysam.asTuple(), threads=WORKERS)
+            # self._n_of_variants: int = sum(1 for _ in f.fetch())  # TODO: Make this more efficient
+            # f.close()
+            self._n_of_variants: int = int(p.stdout.read().strip())  # TODO: Handle error
         except (subprocess.CalledProcessError, ValueError, OSError):
             # bcftools returned 1, or couldn't find number of records, or couldn't find index
             raise ValueError  # Consolidate to one exception type
