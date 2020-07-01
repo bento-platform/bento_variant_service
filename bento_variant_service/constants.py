2366fab2d3386ac4deb6d80803cc811285078370
David Lougheed <david.lougheed@gmail.com>
2020-07-01 11:33:38 -0400

    Add service artifact constant

--- bento_variant_service/constants.py
+++ bento_variant_service/constants.py
@@ -5,6 +5,7 @@ __all__ = [
     "CHORD_URL",
     "DRS_URL_BASE_PATH",
     "SERVICE_NAME",
+    "SERVICE_ARTIFACT",
     "SERVICE_TYPE",
     "SERVICE_ID",
 ]
@@ -16,5 +17,6 @@ CHORD_URL = os.environ.get("CHORD_URL", "http://127.0.0.1:5000/")
 DRS_URL_BASE_PATH = os.environ.get("DRS_URL_BASE_PATH", "/api/drs")
 
 SERVICE_NAME = "Bento Variant Service"
-SERVICE_TYPE = "ca.c3g.bento:variant:{}".format(__version__)
+SERVICE_ARTIFACT = "variant"
+SERVICE_TYPE = f"ca.c3g.bento:{SERVICE_ARTIFACT}:{__version__}"
 SERVICE_ID = os.environ.get("SERVICE_ID", SERVICE_TYPE)
