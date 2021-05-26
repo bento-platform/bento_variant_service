ca87c519a43a9340f3048dbf855b6c3a4768d33e
David Lougheed <david.lougheed@gmail.com>
2021-05-26 15:44:20 -0400

    tests: more DRS testing

--- tests/test_drs_utils.py
+++ tests/test_drs_utils.py
@@ -1,10 +1,22 @@
+import os
+import responses
+
 from bento_variant_service.tables.vcf.drs_manager import DRSVCFTableManager
 from bento_variant_service.tables.vcf.drs_utils import drs_vcf_to_internal_paths
-from .shared_data import DRS_VCF_ID, DRS_IDX_ID
+
+from .shared_data import (
+    VCF_TEN_VAR_FILE_PATH,
+    VCF_TEN_VAR_INDEX_FILE_PATH,
+
+    DRS_VCF_ID,
+    DRS_IDX_ID,
+    DRS_VCF_RESPONSE,
+    DRS_IDX_RESPONSE,
+)
 
 
 # noinspection PyUnusedLocal
-def test_drs_vcf_to_internal_paths(client_drs_mode, drs_table_manager: DRSVCFTableManager):
+def test_drs_vcf_to_internal_paths_errors(client_drs_mode, drs_table_manager: DRSVCFTableManager):
     assert drs_vcf_to_internal_paths(
         f"http://drs.local/objects/{DRS_VCF_ID}",
         f"http://drs.local/objects/{DRS_IDX_ID}",
@@ -21,4 +33,36 @@ def test_drs_vcf_to_internal_paths(client_drs_mode, drs_table_manager: DRSVCFTab
         f"drs://drs.local/{DRS_IDX_ID}",
     ) is None
 
-    # TODO: VALID CASE
+
+# noinspection PyUnusedLocal
+@responses.activate
+def test_drs_vcf_to_internal_paths_404(client_drs_mode, drs_table_manager: DRSVCFTableManager):
+    responses.add(responses.GET, f"http://drs.local/objects/{DRS_VCF_ID}", json={
+        "status_code": 404,
+        "message": "Not found",
+    }, status=404)
+    responses.add(responses.GET, f"http://drs.local/objects/{DRS_IDX_ID}", json={
+        "status_code": 404,
+        "message": "Not found",
+    }, status=404)
+
+    assert drs_vcf_to_internal_paths(
+        f"drs://drs.local/{DRS_VCF_ID}",
+        f"drs://drs.local/{DRS_IDX_ID}",
+    ) is None
+
+
+# noinspection PyUnusedLocal
+@responses.activate
+def test_drs_vcf_to_internal_paths_valid(client_drs_mode, drs_table_manager: DRSVCFTableManager):
+    responses.add(responses.GET, f"http://drs.local/objects/{DRS_VCF_ID}", json=DRS_VCF_RESPONSE)
+    responses.add(responses.GET, f"http://drs.local/objects/{DRS_IDX_ID}", json=DRS_IDX_RESPONSE)
+
+    r = drs_vcf_to_internal_paths(
+        f"drs://drs.local/{DRS_VCF_ID}",
+        f"drs://drs.local/{DRS_IDX_ID}",
+    )
+
+    assert r[:2] == (os.path.abspath(VCF_TEN_VAR_FILE_PATH), os.path.abspath(VCF_TEN_VAR_INDEX_FILE_PATH))
+    assert r[2] is None
+    assert r[3] is None
